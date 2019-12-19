8b63a870a77b275c435b984ea75ea756e537c1c2
chen <1728970104@qq.com>
2019-12-19 19:36:56 +0800

    完成登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,9 @@
 // 导入vue
 import Vue from 'vue'
-// 从element-ui这个里面按需导入所需要的组件
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 // 通过vue.use()挂载所导入的组件
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
